4b82587ecda2799ebdf4c6f659d9fe6966f873c0
EC <capelle.e@gmail.com>
2016-08-12 13:30:13 +0200

    Start mysql adapter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const Shrinku = require('./shrinku');
 const shrinku = new Shrinku();
 
-shrinku.addAdapter('memory', new Shrinku.Adapters.MemoryAdapter());
-// shrinku.addAdapter('mysql', new Shrinku.Adapters.MySQLAdapter({
-//   host: 'localhost',
-//   user: 'loot',
-//   password: 'loot',
-//   database: 'shrinku_test',
-//   connectionLimit: 50,
-//   useTransation: {
-//     connectionLimit: 1
-//   }
-// }));
+// shrinku.addAdapter('memory', new Shrinku.Adapters.MemoryAdapter());
+shrinku.addAdapter('mysql', new Shrinku.Adapters.MySQLAdapter({
+  host: 'localhost',
+  user: 'loot',
+  password: 'loot',
+  database: 'shrinku_test',
+  connectionLimit: 50,
+  useTransation: {
+    connectionLimit: 1
+  }
+}));
 
 shrinku
   .shrink({ url : 'http://google.be' })
